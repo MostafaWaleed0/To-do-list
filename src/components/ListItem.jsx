ef43b636e61264a01d8ac0c1748c3148c1ef60c6
Mostafa Waleeed <mostafawaleed206@gmail.com>
2023-01-04 10:07:11 +0200

    ✨feat: set `background-color` and size to input

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -8,7 +8,12 @@ export default function ListItem({
   checkedNote,
 }) {
   const input = (
-    <input type="checkbox" onChange={checkedNote} checked={checked} />
+    <input
+      type="checkbox"
+      onChange={checkedNote}
+      checked={checked}
+      className="accent-[#e300be] h-6 w-6"
+    />
   );
 
   return (
